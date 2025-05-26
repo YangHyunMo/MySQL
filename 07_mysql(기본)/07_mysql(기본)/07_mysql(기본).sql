5aab14b7d982bf7a2d55794139a409adc231a936
Mo <yanghmo98@naver.com>
2025-05-26 17:29:22 +0900

    07 Java 연동 JDBC 프로그래밍 기본문제 풀이

--- 07_mysql(기본)/07_mysql(기본)/07_mysql(기본).sql
+++
@@ -1,9 +0,0 @@
--- [문제 1] 다음과 같이 데이터베이스를 준비하세요
-CREATE DATABASE jdbc_ex;
-
-drop user 'scoula'@'localhost';
-CREATE USER 'scoula'@'%' IDENTIFIED BY '1234';
-
-GRANT ALL PRIVILEGES ON jdbc_ex.* TO 'scoula'@'%';
-FLUSH PRIVILEGES;
-SELECT * FROM mysql.userusers;
